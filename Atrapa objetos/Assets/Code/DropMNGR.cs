91c903a9ddd24c7a1cf0f517aff0b0154fbd1634
3dstudiobag <71454549+TioPistolas@users.noreply.github.com>
2024-04-02 13:40:22 -0600

    Cambios 02/04/2024

---
+++ Atrapa objetos/Assets/Code/DropMNGR.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropMNGR : MonoBehaviour
+{
+    public float timerDrop; //El tiempo que va a dropear cada objeto
+    public GameObject collect; //El coleccionable
+    public GameObject powerUp; //El powerUp
+    public GameObject spawner; //El spawn de los objetos
+    public int amountSpawners; //La cantidad de spawners que tendremos en la escena
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
