615fe040d7b6938928a56b7c76dcd9b64a596947
3dstudiobag <71454549+TioPistolas@users.noreply.github.com>
2024-04-04 16:26:35 -0600

    Cambios del 4 de Abril
    Complete la mecanica de los objetos que caen, falta arreglar cosas como el collider del jugador y que el jugador las atrape

--- Atrapa objetos/Assets/Code/DropMNGR.cs
+++ Atrapa objetos/Assets/Code/DropMNGR.cs
@@ -10,6 +10,9 @@ public class DropMNGR : MonoBehaviour
     public GameObject spawner; //El spawn de los objetos
     public int amountSpawners; //La cantidad de spawners que tendremos en la escena
 
+    [SerializeField] private float minTras;
+    [SerializeField] private float maxTras;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +27,15 @@ public class DropMNGR : MonoBehaviour
 
     IEnumerator aceiteSpawn()
     {
-        yield return new WaitForSeconds(timerDrop);
+        while (true)
+        {
+            var wanted = Random.Range(minTras, maxTras);
+            var position = new Vector3(wanted, transform.position.y);
+            GameObject gameObject = Instantiate(aceiteCollect[Random.Range(0, aceiteCollect.Length)], position, Quaternion.identity);
+            yield return new WaitForSeconds(timerDrop);
+            Destroy(gameObject, 5f);
+        }
+        
     }
     
  
