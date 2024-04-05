e8884076887834443848adc1496875b4fce2bf84
ALau00 <anasoriano0609@gmail.com>
2024-04-05 17:19:27 -0600

    Implementación de Wii [05/04/24]

--- Atrapa objetos/Assets/Code/Player.cs
+++ Atrapa objetos/Assets/Code/Player.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WiimoteApi;
 
 public class Player : MonoBehaviour
 {
+    private Wiimote wiimote;
     public float movementSpeed; //La velocidad del Player
     public float dirX;
     
     void Start()
     {
-       
+       InitWiimotes();
     }
 
    
@@ -18,5 +20,54 @@ public class Player : MonoBehaviour
         dirX = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * dirX * movementSpeed);
 
+        if (!WiimoteManager.HasWiimote()) { return; }
+        
+        wiimote = WiimoteManager.Wiimotes[0];
+
+        int ret;
+         do
+	    {
+            ret = wiimote.ReadWiimoteData();
+
+            //AGREGAR EL TRANSLATE AQUI (Algo así?)
+            print(GetAccelVector());
+            //transform.Translate(GetAccelVector().x * dirX * movementSpeed);
+            
+	    } while (ret > 0);
+    }
+
+
+
+    //WIIMOTE
+    void InitWiimotes() // Busca WiiMotes & Cambia LEDs
+    {
+	    WiimoteManager.FindWiimotes();
+
+	    foreach(Wiimote remote in WiimoteManager.Wiimotes) {
+            remote.Accel.CalibrateAccel(0);
+            remote.SendPlayerLED(true, false, false, false);
+            remote.SetupIRCamera(IRDataType.EXTENDED);
+            //remote.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL);
+	    }
+    }
+
+    void CleanWiimotes() //Desconecta Wiimotes
+    {
+        WiimoteManager.Cleanup(wiimote);
+        wiimote = null;
+    }
+
+    private Vector3 GetAccelVector()
+    {
+        float accel_x;
+        float accel_y;
+        float accel_z;
+
+        float[] accel = wiimote.Accel.GetCalibratedAccelData();
+        accel_x = accel[0];
+        accel_y = -accel[2];
+        accel_z = -accel[1];
+
+        return new Vector3(accel_x, accel_y, accel_z);
     }
 }
