ff20258cd4f684169d72c87f34671486ea7cf3f7
3dstudiobag <71454549+TioPistolas@users.noreply.github.com>
2024-04-19 09:01:43 -0600

    Mecanica Score
    Implemente la mecanica de agarrar el coleccionable

---
+++ Atrapa objetos/Assets/Code/SistemaRecoleccion.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SistemaRecoleccion : MonoBehaviour
+{
+    public int cantidadCollecionable;
+    public TextMeshProUGUI score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = cantidadCollecionable.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Collector1"))
+        {
+            Destroy(collision.gameObject);
+            cantidadCollecionable++;
+        }
+    }
+}
