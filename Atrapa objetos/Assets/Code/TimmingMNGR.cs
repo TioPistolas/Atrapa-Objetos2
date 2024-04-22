5555cf3dc76bf055809e4d6818019dabe585bb29
3dstudiobag <71454549+TioPistolas@users.noreply.github.com>
2024-04-22 15:25:38 -0600

    Mecanica timmer
    Añadi la mecanica del timmer pero no se refleja en el texto del UI :;

--- Atrapa objetos/Assets/Code/TimmingMNGR.cs
+++ Atrapa objetos/Assets/Code/TimmingMNGR.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TimmingMNGR : MonoBehaviour
 {
+    public float tiempoMaximo;
+
+    public float timmerActual;
+
+    public bool tiempoActivado;
+
+    public TextMeshProUGUI timmerText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ActivarTemporizador();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (tiempoActivado)
+        {
+            cambiarContador();
+        }
+    }
+
+    public void cambiarContador()
+    {
+        timmerActual -= Time.deltaTime;
+        if(timmerActual <= 0)
+        {
+            Debug.Log("derrota");
+            cambiarTemporizador(false);
+        }
+    }
+
+    public void cambiarTemporizador(bool estado)
+    {
+        tiempoActivado = estado;
+    }
+    
+    public void ActivarTemporizador()
+    {
+        timmerActual = tiempoMaximo;
+        cambiarTemporizador(true);
+        timmerText.text = timmerActual.ToString();
+    }
+
+    public void desactivarTemporizador()
+    {
+        cambiarTemporizador(false);
     }
 }
